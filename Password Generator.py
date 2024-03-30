17b0bd0cbdf51e9b396660b22342fa5ffb4c6fad
Reece O'Leary <91633093+Reece-OL@users.noreply.github.com>
2024-03-30 13:11:25 +0000

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,6 +1,6 @@
 import random
 
-char = '~`!@#$%^&*()-_+=[]|\/:;"<>,.?1234567890'
+char = '~`!@#$%^&*()-_+=[]|\/:;"<>,.?1234567890AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
 fileName = input('File name: ')
 fileName += ".txt"
 passwordLength = input('Password Length: ')
@@ -9,4 +9,4 @@ passwordLength = int(passwordLength)
 password = ''.join(random.choices(char, k=passwordLength))
 
 with open(fileName, 'w') as file:
-    file.write(password)  
+    file.write(password) 
