bf07b9226f244bb225cf75bb1322f71614025f86
Reece O'Leary <91633093+Reece-OL@users.noreply.github.com>
2024-09-23 10:32:34 +0100

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,12 +1,25 @@
-import random
+ import random
 
-char = '~`!@#$%^&*()-_+=[]|\/:;"<>,.?1234567890AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
-fileName = input('File name: ')
-fileName += ".txt"
-passwordLength = input('Password Length: ')
-passwordLength = int(passwordLength)
 
-password = ''.join(random.choices(char, k=passwordLength))
+char = '~`!@#$%^&*()-_+=[]|\\/:;"<>,.?1234567890AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
 
-with open(fileName, 'w') as file:
-    file.write(password) 
+# generates 5 letter word
+def generate_word():
+    return ''.join(random.choices(char, k=5))
+
+# Gset a file name based on user input
+file_name = input('File name: ') + ".txt"
+
+# generates three words
+word1 = generate_word()
+word2 = generate_word()
+word3 = generate_word()
+
+# sets three 5 letter password
+password = f"{word1} {word2} {word3}"
+
+# store the pasword in a txt file within teh directory 
+with open(file_name, 'w') as file:
+    file.write(password)
+
+print(f"Password saved in {file_name}")
